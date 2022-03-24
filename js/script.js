dafa71bcd7822d8485f4b6ccaf5779e7c29137f6
GuilhermeSchuch <guiguischuch@gmail.com>
2022-03-24 15:17:15 -0300

    Updates

--- js/script.js
+++ js/script.js
@@ -5,11 +5,13 @@ let $input = document.createElement("input")
 let $span = document.createElement("span");
 let $ionIcon = document.createElement("ion-icon");
 let $img = document.createElement("img");
+let opacidade = document.createElement("div");
 
 //Atributos
 $input.setAttribute("type", "checkbox");
 $ionIcon.setAttribute("name", "bulb-outline");
 $img.setAttribute("src", "img\\bulb_off.jpg");
+opacidade.setAttribute("class", "opacidade-on");
 
 //Appends
 $span.append($ionIcon);
@@ -17,14 +19,19 @@ $label.append($input);
 $label.append($span);
 $body.append($label);
 $body.append($img);
+$body.append(opacidade);
 
 //Função
 $input.onchange = function(){
 
     if($img.getAttribute("src") == "img\\bulb_off.jpg"){
         $img.setAttribute("src", "img\\bulb_on.jpg");
+        $body.setAttribute("class", "background-white");
+        opacidade.setAttribute("class", "opacidade-off");
     }
     else{
         $img.setAttribute("src", "img\\bulb_off.jpg");
+        $body.setAttribute("class", "background-black");
+        opacidade.setAttribute("class", "opacidade-on");
     }
 }
